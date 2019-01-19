5e4406364a302fe5f1a8736dd9b4b442c17e2f7b
Grady Whelan <grady.whelan@gmail.com>
2019-01-18 17:34:56 -0800

    Added second solenoid

--- Robot2019/src/main/java/frc/team2813/robot/OI.java
+++ Robot2019/src/main/java/frc/team2813/robot/OI.java
@@ -10,11 +10,12 @@ public class OI {
 	public static Joystick joystick;
 	public static final int JOYSTICK_ID = 0;
 	private final Solenoid solenoid = new Solenoid(0,1);
+	private final Solenoid solenoid1 = new Solenoid(0,2);
 	
 	public OI() {
 		joystick = new Joystick(JOYSTICK_ID);
 		new JoystickButton(joystick, 1).whenPressed(new ToggleSolenoid(solenoid));
-		
+		new JoystickButton(joystick, 2).whenPressed(new ToggleSolenoid(solenoid1));
 		
 		
 	}
